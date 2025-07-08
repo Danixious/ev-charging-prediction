7413f7b8fa937893affe8a38fdc4467b8e3be4cf
Danixious <danieljuliusnatal@gmail.com>
2025-07-08 12:04:47 +0530

    EDA #1

---
+++ notebooks/EDA.py
@@ -0,0 +1,50 @@
+import pandas as pd
+from datetime import datetime,date,time
+import matplotlib.pyplot as plt
+import seaborn as sns
+for chunk in pd.read_csv("D:/EV_prediction/ev-charging-prediction/data/Aggregated_ev_data.csv",chunksize = 500000 ):
+    print(chunk.head())
+    print(chunk.info())
+    print(chunk.describe())
+    break
+
+for chunk in pd.read_csv("D:/EV_prediction/ev-charging-prediction/data/Aggregated_ev_data.csv",chunksize = 500000 ):
+    chunk['time'] = pd.to_datetime(chunk['time'])
+
+    chunk['hour'] = chunk['time'].dt.hour
+    chunk['weekday'] = chunk['time'].dt.day_name()
+    chunk['month'] = chunk['time'].dt.month
+    chunk['is_weekend'] = chunk['time'].dt.dayofweek >= 5
+
+    print(chunk[['time','hour','weekday','month','is_weekend']].head())
+    break
+
+avg_hour = chunk.groupby('hour')['volume'].mean().reset_index()
+
+plt.figure(figsize=(10, 5))
+plt.bar(avg_hour['hour'], avg_hour['volume'], color='skyblue')
+plt.title("Average Charging Volume by Hour of Day")
+plt.xlabel("Hour of Day")
+plt.ylabel("Average Volume (kWh)")
+plt.xticks(range(0, 24))  
+plt.grid(axis='y')
+plt.tight_layout()
+plt.show()
+
+
+weekday_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+chunk['weekday'] = pd.Categorical(chunk['weekday'], categories=weekday_order, ordered=True)
+
+
+weekday_avg = chunk.groupby('weekday')['volume'].mean().reset_index()
+
+
+plt.figure(figsize=(10, 5))
+sns.barplot(data=weekday_avg, x='weekday', y='volume', palette='Oranges')
+plt.title("Average Charging Volume by Weekday", fontsize=14)
+plt.xlabel("Day of the Week")
+plt.ylabel("Average Volume (kWh)")
+plt.xticks(rotation=45)
+plt.grid(axis='y')
+plt.tight_layout()
+plt.show()
--- notebooks/agg.py
+++ notebooks/agg.py
@@ -2,9 +2,9 @@ import pandas as pd
 from pathlib import Path
 
 path2csv = Path("D:/EV_prediction/ev-charging-prediction/data/UrbanEVDataset/UrbanEVDataset/20220901-20230228_station-raw/charge_5min")
-csvlist = path2csv.glob("*.csv")
+csvlist = list(path2csv.glob("*.csv"))
 
-
+required_columns = (['time','volume','busy','idle','fast_busy','slow_busy','s_price','e_price','duration'])
 def check_column(csvlist,column_name):
     try:
         for files in csvlist:
@@ -21,5 +21,28 @@ column_name = "time"
 if check_column(csvlist,column_name):
     print("ok report")
 
-df = pd.DataFrame({'time','volume','busy','idle','fast_busy','slow_busy','s_price','e_price','duation'})
-print(df)
+    dfs = []
+    for file in csvlist:
+        try:
+            df = pd.read_csv(file)
+            df = df[required_columns]
+            dfs.append(df)
+        except Exception as e:
+            print(f"Error parsing {file.name}: {e}")
+    full_df = pd.concat(dfs, ignore_index=True)
+    print("Combined DataFrame shape: ",full_df.shape)
+else: 
+    print("aborting.....")
+
+aggregated_df = full_df.groupby('time').agg({
+    'volume': 'sum',
+    'busy': 'sum',
+    'idle': 'sum',
+    'fast_busy': 'sum',
+    'slow_busy': 'sum',
+    's_price': 'mean',
+    'e_price': 'mean',
+    'duration': 'mean'
+}).reset_index()
+
+full_df.to_csv("Aggregated_ev_data.csv",index = False)
